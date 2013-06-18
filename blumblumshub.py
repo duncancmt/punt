ba927c9c4e1bac0204ba781fce243f7e94c761cb
Duncan Townsend <duncant@mit.edu>
2013-06-18 17:57:08 -0400

    Initial commit of Tetazoo Puntlist

---
+++ blumblumshub.py
@@ -0,0 +1,279 @@
+from __future__ import division
+
+import decimal
+from decimal import Decimal
+
+import math
+import random
+import struct
+import cPickle
+from fractions import gcd
+
+import primes
+from sieve import Sieve
+
+try:
+    from gmpy2 import mpz
+    has_gmpy = True
+except ImportError:
+    try:
+        from gmpy import mpz
+        has_gmpy = True
+    except ImportError:
+        has_gmpy = False
+
+def lcm(a,b):
+    return (a * b) // gcd(a,b)    
+    
+class BlumBlumShubRandom(random.Random):
+    def __init__(self, security, bits_to_supply=2**20, tolerance=0.5,
+                 seed=None, paranoid=True, _state=None):
+        """
+        Arguments:
+            security: The log (base 2) of the number of operations required to distinguish BBS output from random noise.
+            tolerance: The difference between the probability that the sequence produced by BBS be declared non-random and the probability that a truly random sequence will be declared non-random.
+            bits_to_supply: The number of bits that this RNG will be expected to produce securely. Default: 2**20
+            seed: Deterministically initializes the RNG. Must be a long or integer with length 2*security. (Optional)
+            paranoid: If paranoid is true, this RNG will raise a RuntimeError if asked to supply >bits_to_supply bits
+            _state: If supplied, we skip normal initialization and use the supplied state instead. (Optional)
+        """
+        if _state is not None:
+            self.setstate(_state)
+        else:
+            self.security = security
+            self.paranoid = paranoid
+            self._bit_count = 0L
+            self._bit_count_limit = bits_to_supply
+            self._cache = 0L
+            self._cache_len = 0L
+            
+            (self._modulus_length, self._bits_per_iteration) = self.optimal_parameters(self.security, self._bit_count_limit, tolerance)
+
+            # python random.SystemRandom() is unsuitable because it uses /dev/urandom
+            if seed is None:
+                seed = 0L
+                with open('/dev/random','r') as randfile:
+                    for i in xrange(int(math.ceil(2*self.security / 8.0))):
+                        seed |= struct.unpack('B', randfile.read(1))[0] << i*8
+            self.seed(seed)
+
+    @classmethod
+    def from_state(cls, state):
+        """Create a BlumBlumShubRandom instance from a state tuple"""
+        return cls(security=None, bits_to_supply=None, tolerance=None,
+                   seed=None, paranoid=None, _state=state)
+
+    def random(self):
+        return float(self.getrandbits(53)) / 2**53
+
+    def getrandbits(self,n):
+        if (not self.paranoid) or self._bit_count + n < self._bit_count_limit:
+            self._bit_count += n
+        else:
+            raise RuntimeError("Too many bits supplied. Security guarantees invalidated.")
+        
+        iterations = max(int(math.ceil((1.0*n-self._cache_len) / self._bits_per_iteration)),0)
+
+        mask = 2**self._bits_per_iteration - 1
+        for i in xrange(iterations):
+            self._cache <<= self._bits_per_iteration
+            self._cache |= self.state & mask
+            self._cache_len += self._bits_per_iteration
+            self.state = pow(self.state, 2, self.modulus)
+
+        result = self._cache & ((1<<n) - 1)
+        self._cache >>= n
+        self._cache_len -= n
+        return result
+
+    def seed(self, seed):
+        assert isinstance(seed, (int, long))
+        self.state = x_0 = seed >> self.security
+        random.seed(seed & (2**self.security - 1))
+
+        p = self.gen_special_prime(self._modulus_length / 2, certainty=self.security, random=random)
+        q = self.gen_special_prime(self._modulus_length / 2, certainty=self.security, random=random)
+        random.seed()
+
+        assert p != q
+        self.modulus = p*q
+        self.skip_modulus = lcm(p-1, q-1) # TODO: it probably isn't safe to keep this around
+
+        if has_gmpy:
+            self.modulus = mpz(self.modulus)
+            self.skip_modulus = mpz(self.skip_modulus)
+            self.state = mpz(self.state)
+
+        # degeneracy test as described by
+        # http://www.ciphersbyritter.com/NEWS6/BBS.HTM
+        self.jumpahead(1)
+        assert self.state > 0
+        x_1 = self.state
+        self.jumpahead(1)
+        x_2 = self.state
+        assert x_0 != x_1 and x_1 != x_2 and x_2 != x_0, \
+               "Given parameters resulted in a degenerate cycle. Choose another initial state."
+
+    def getstate(self):
+        return (self.security, self.paranoid,
+                self._bit_count, self._bit_count_limit,
+                self._cache, self._cache_len,
+                self._modulus_length, self._bits_per_iteration,
+                int(self.modulus), int(self.state),
+                int(self.skip_modulus) if self.skip_modulus is not None else None)
+
+    def setstate(self, state):
+        (self.security, self.paranoid,
+         self._bit_count, self._bit_count_limit,
+         self._cache, self._cache_len,
+         self._modulus_length, self._bits_per_iteration,
+         self.modulus, self.state, self.skip_modulus) = state
+
+        if has_gmpy:
+            self.modulus = mpz(self.modulus)
+            self.state = mpz(self.state)
+            if self.skip_modulus is not None:
+                self.skip_modulus = mpz(self.skip_modulus)
+
+    def jumpahead(self, n):
+        if self.skip_modulus is not None:
+            self.state = pow(self.state, pow(2, n, self.skip_modulus), self.modulus)
+        else:
+            for _ in xrange(n):
+                self.state = pow(self.state, 2, self.modulus)
+
+    @staticmethod
+    def gen_special_prime(bits, certainty=128, random=random,
+                          callback=lambda loops, p2_pass, p1_pass, done: None,
+                          callback_period=2**13): # run callback roughly once an hour
+        # In addition to the well-known constraint that p must be congruent
+        # to 3, mod 4, in order to avoid short cycles, we impose the
+        # additional constraints described here:
+        # http://www.ciphersbyritter.com/NEWS6/BBS.HTM
+        # Essentially, p must be a "doubly safe" prime: p1 := (p-1)/2, p2 := (p1-1)/2
+        # where p1 and p2 are also prime
+
+        # Only numbers with particular residues can generate a doubly safe prime.
+        # By only generating candidates with those particular residues, we
+        # dramatically reduce our search space.
+        sieve = Sieve(max(bits-certainty,64))
+
+        loops = 0
+        p2_pass = 0
+        p1_pass = 0
+
+        # TODO: adjust certainty to account for the number of tests that we run
+        while True:
+            loops += 1
+            if loops % callback_period == 0:
+                callback(loops, p2_pass, p1_pass, False)
+
+            # choose a random p2 that has an allowed residue
+            p2 = sieve.make_candidate(bits - 2, random) # we get the low two bits by left shifting and adding one, twice
+            p1 = p2 * 2 + 1
+            p = p1 * 2 + 1
+            # first run a few abbreviated Miller-Rabin tests to fail quickly
+            if primes.mr_test(p2, rounds=1) \
+                   and primes.mr_test(p1, rounds=1) \
+                   and primes.mr_test(p, rounds=1):
+                if primes.mr_test(p2, certainty=certainty):
+                    p2_pass += 1
+                    if primes.mr_test(p1, certainty=certainty):
+                        p1_pass += 1
+                        if primes.mr_test(p, certainty=certainty):
+                            break
+        callback(loops, p2_pass, p1_pass, True)
+        return p
+    
+    @staticmethod
+    def optimal_parameters(log_Ta, M, epsilon, maxn=2**32):
+        """
+        Arguments:
+            log_Ta: Log (base 2) of the number of operations required to distinguish BBS output from random noise.
+            M: Number of bits that this instance of BBS will output. If more than M bits are extracted from this instance, security guarantees are invalidated.
+            epsilon: Also called the tolerance. The difference between the probability that the sequence produced by BBS be declared non-random and the probability that a truly random sequence will be declared non-random.
+            maxn: Maximum bit-length of the modulus. Default: 2**32
+        Returns:
+            A tuple of (bit-length of modulus (n), bits to extract per cycle (j))
+        """
+    
+        log_Ta = Decimal(log_Ta)
+        M = Decimal(M)
+        epsilon = Decimal(epsilon)
+        maxn = Decimal(maxn)
+    
+        def attack_difficulty(n, j):
+            n = Decimal(n)
+            j = Decimal(j)
+            ln2 = Decimal(2).ln()
+            
+            # number of operations required by GNFS to factor an integer of bit length n
+            # Equation (8) in http://www.win.tue.nl/~berry/papers/ima05bbs.pdf recommends
+            # gamma = 2.8e-3. However, ECRYPT is a better authority on the subject.
+            # Section 6.2.1 of ECRYPT 2012 suggests gamma = 2**-14 to 2**-12
+            # See: http://www.ecrypt.eu.org/documents/D.SPA.20.pdf
+            gamma = Decimal(1)/2**14
+            tmp1 = (Decimal(64)/9)**(Decimal(1)/3)
+            tmp2 = (n*ln2)**(Decimal(1)/3)
+            tmp3 = ((n*ln2).ln())**(Decimal(2)/3)
+            L = gamma*(tmp1*tmp2*tmp3).exp()
+
+            # difficulty of distinguishing BBS from a random sequence
+            # See equation (9) in http://www.win.tue.nl/~berry/papers/ima05bbs.pdf
+            delta = epsilon/((2**j - 1) * M)
+            left = (L * delta**2)/(36 * n * (n.ln()/ln2))
+            right = 2**(2*j+9) * n / (delta**4)
+            return left - right
+    
+        def find_n(j):
+            upper = maxn
+            lower = 2**j
+            n = None
+            while upper > lower:
+                n = (upper + lower)/2
+                n = n.to_integral_value(decimal.ROUND_CEILING)
+                if attack_difficulty(n, j) > 2**log_Ta:
+                    if n == upper:
+                        # because of the ceiling, we can get stuck here
+                        break
+                    upper = n
+                else:
+                    lower = n
+                    
+            n += n % 2 # force n to be even
+            assert 2**log_Ta <= attack_difficulty(n, j)
+            return n
+        
+        def work_per_bit(j):
+            # See section 7.3 in http://www.win.tue.nl/~berry/papers/ima05bbs.pdf
+            n = find_n(j)
+            return M*n**2/j
+    
+        with decimal.localcontext() as ctx:
+            ctx.prec = 200
+            maxj = (maxn.ln()/Decimal(2).ln()).to_integral_value(decimal.ROUND_FLOOR)
+            upper = maxj+1 # we can never actually select this value because of the floor
+            lower = Decimal(1)
+            j = None
+            # valley finding
+            while upper > lower:
+                j = (upper + lower)/2
+                j = j.to_integral_value(decimal.ROUND_FLOOR)
+                work = work_per_bit(j)
+                if work_per_bit(j-1) < work:
+                    upper = j
+                elif work_per_bit(j+1) < work:
+                    if lower == j:
+                        # because of the floor, we can get stuck here
+                        break
+                    lower = j
+                else:
+                    break
+
+            n = find_n(j) # TODO: this does extra work
+            assert 2**log_Ta <= attack_difficulty(n, j)
+            assert work_per_bit(j-1) >= work_per_bit(j) or j == maxj
+            assert work_per_bit(j+1) >= work_per_bit(j) or j == maxj
+            n = int(n)
+            j = int(j)
+            return (n, j)
---
+++ find_special_prime.py
@@ -0,0 +1,154 @@
+import cProfile
+import cPickle
+import time
+import sys
+import math
+import multiprocessing
+import select
+import operator
+import signal
+import errno
+from blumblumshub import BlumBlumShubRandom
+from sieve import Sieve
+from random import SystemRandom
+random = SystemRandom()
+
+status_filename = (sys.argv[1] if len(sys.argv) > 1 else 'status.pkl')
+
+try:
+    old_loops, old_p2_pass, old_p1_pass = cPickle.Unpickler(open(status_filename,'r')).load()
+except IOError as e:
+    if e.errno == errno.ENOENT:
+        old_loops, old_p2_pass, old_p1_pass = (0, 0, 0)
+    else:
+        raise
+
+class AllDone(Exception):
+    pass
+
+class TimeoutException(Exception):
+    pass
+
+def print_status(statuses, force=False):
+    global lasttime
+    now = time.time()
+    intermediate_statuses = filter(lambda x: isinstance(x, tuple), statuses)
+    real_loops = sum(map(operator.itemgetter(0), intermediate_statuses))
+    loops = real_loops + old_loops
+    p2_pass = sum(map(operator.itemgetter(1), intermediate_statuses)) + old_p2_pass
+    p1_pass = sum(map(operator.itemgetter(2), intermediate_statuses)) + old_p1_pass
+    done = reduce(operator.or_, map(operator.itemgetter(3), intermediate_statuses), False)
+    result = filter(lambda x: isinstance(x, (int, long)), statuses)
+
+    # binomial expansion with 4 terms
+    # TODO: this is imprecise for small values of bits
+    candidate_probability = 1/(((math.log(2)*bits)**3)*sieve.advantage)
+    prob = (loops * candidate_probability)
+    prob -= float(loops*(loops-1))/2 * candidate_probability**2
+    prob += float(loops*(loops-1)*(loops-2))/6 * candidate_probability**3
+    prob -= float(loops*(loops-1)*(loops-2)*(loops-3))/24 * candidate_probability**4
+
+    if done:
+        print >>sys.stderr, "Found doubly safe prime after %s iterations (probability %s)." % (loops, prob)
+        print >>sys.stderr, "We found %s primes and %s singly safe primes." % (p2_pass, p1_pass)
+    if len(result) > 0:
+        print result[0]
+        raise AllDone
+    elif force or now - lasttime > 3600:
+        print >>sys.stderr, "%s candidates tested" % loops
+        print >>sys.stderr, "%s primes found" % p2_pass
+        print >>sys.stderr, "%s safe primes found" % p1_pass
+        print >>sys.stderr, "%s expected probability of finding prime already" % prob
+        print >>sys.stderr, "%s iterations per second" % (real_loops / (now - starttime))
+        print >>sys.stderr, "%s effective guesses per second" % ((real_loops / (now - starttime)) / sieve.advantage)
+        print >>sys.stderr, "%s seconds since last output" % (now - lasttime)
+        print >>sys.stderr
+        lasttime = now
+
+    
+if __name__ == "__main__":
+    bits = 8192
+    certainty = 256
+    sieve = Sieve(max(bits-certainty,64))
+    gsp = BlumBlumShubRandom.gen_special_prime
+
+    print >>sys.stderr, "Trying to find a special prime with %s bits using a sieve of index %s, size %s, advantage %s" % (bits, sieve.index, math.log(sieve.modulus,2), sieve.advantage)
+
+    # figure out how often to check in
+    checkin_seconds = 5
+    checkin_iterations = None
+    def handler(*args):
+        raise TimeoutException
+    signal.signal(signal.SIGALRM, handler)
+    signal.alarm(checkin_seconds)
+    while checkin_iterations is None:
+        checkin_seconds *= 2
+        try:
+            def trial_callback(loops, p2_pass, p1_pass, done):
+                global checkin_iterations
+                checkin_iterations = loops
+            print gsp(bits, certainty, random=random, callback=trial_callback, callback_period=1)
+            sys.exit(0)
+        except TimeoutException:
+            pass
+    print >>sys.stderr, "Subprocesses will check in every %s iterations (%s seconds)" % (checkin_iterations, checkin_seconds)
+    signal.signal(signal.SIGALRM, signal.SIG_DFL)
+
+    starttime = time.time()
+    lasttime = starttime
+
+    def callback(pipe):
+        return (lambda *args: pipe.send(args))
+    def worker(pipe):
+        def thunk():
+            signal.signal(signal.SIGINT, signal.SIG_IGN) # ignore Ctrl-C
+            signal.signal(signal.SIGQUIT, signal.SIG_IGN) # ignore Ctrl-\
+            pipe.send(gsp(bits, certainty, random=random,
+                          callback=callback(pipe), callback_period=checkin_iterations))
+            sys.exit(0)
+        return thunk
+
+    parent_pipes = [None]*multiprocessing.cpu_count()
+    processes = [None]*len(parent_pipes)
+    epoll = select.epoll()
+    for i in xrange(len(parent_pipes)):
+        parent_pipes[i], child_pipe = multiprocessing.Pipe()
+        epoll.register(parent_pipes[i].fileno(), select.EPOLLIN)
+        processes[i] = multiprocessing.Process(target=worker(child_pipe))
+        processes[i].daemon = True
+        processes[i].start()
+    del child_pipe
+
+    statuses = [None]*len(parent_pipes)
+
+    def handler(*args):
+        print >>sys.stderr
+        print_status(statuses, True)
+    signal.signal(signal.SIGQUIT, handler)
+
+    try:
+        while True:
+            try:
+                epoll.poll()
+            except IOError as e:
+                if e.errno == errno.EINTR: # Interrupted syscall
+                    continue
+                else:
+                    raise
+            for i,parent_pipe in enumerate(parent_pipes):
+                if parent_pipe.poll():
+                    statuses[i] = parent_pipe.recv()
+            print_status(statuses, False)
+    except (AllDone, KeyboardInterrupt):
+        pass
+    finally:
+        print >>sys.stderr
+        print >>sys.stderr, "Time to go! Cleaning up...",
+        intermediate_statuses = filter(lambda x: isinstance(x, tuple), statuses)
+        loops = sum(map(operator.itemgetter(0), intermediate_statuses)) + old_loops
+        p2_pass = sum(map(operator.itemgetter(1), intermediate_statuses)) + old_p2_pass
+        p1_pass = sum(map(operator.itemgetter(2), intermediate_statuses)) + old_p1_pass
+        cPickle.Pickler(open(status_filename,'w'),-1).dump((loops, p2_pass, p1_pass))
+        map(operator.methodcaller("terminate"), processes)
+        print >>sys.stderr, "done."
+
